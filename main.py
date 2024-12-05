26b9113bdb0a005053680e441b2454d443e388a2
StephenSulimani <stephensulimani@gmail.com>
2024-12-04 19:22:42 -0500

    :fire: cleaned up main.py

--- main.py
+++ main.py
@@ -14,13 +14,6 @@ import os
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-# stock_data: pd.DataFrame = pd.DataFrame()
-
-# TICKER = "AMC"
-
-# stock_data = Builder.build(TICKER, "01-01-2024", "12-01-2024", 0.8, 500)
-# stock_data.to_csv(f"data/{TICKER}.csv")
-
 DATASET_LOADED = False
 DATASET: pd.DataFrame = pd.DataFrame()
 
@@ -266,43 +259,3 @@ while True:
         load_dataset()
     elif selection == 3:
         train_model()
-
-
-# encoder = LabelEncoder()
-# stock_data["Signal"] = encoder.fit_transform(stock_data["Signal"])
-
-# label_mapping = {
-#     label: index for index, label in enumerate(cast(np.ndarray, encoder.classes_))
-# }
-
-
-# print(f"Label Encoder Classes: {label_mapping}")
-
-# train_x, train_y, test_x, test_y = split_time_series(stock_data)
-
-# print(f"Train Set Length: {len(train_x)}")
-# print(f"Test Set Length: {len(test_x)}")
-
-# print(f"Train Set Head")
-# print(train_x.head())
-
-# print(f"Train Y Head")
-# print(train_y.head())
-
-# learner = BagLearner(RTLearner, 50, True)
-
-# learner.add_evidence(train_x, train_y)
-
-# print("Successfully Trained")
-
-# y_pred = learner.query(loads(cast(str, test_x.to_json(orient="records"))))
-
-# accuracy, precision, f1, recall, _ = learner.generate_classification_stats(
-#     test_y, y_pred
-# )
-
-# print("Test Results")
-# print(f"Accuracy: {accuracy}")
-# print(f"Precision: {precision}")
-# print(f"F1 Score: {f1}")
-# print(f"Recall: {recall}")
